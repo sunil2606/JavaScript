74808de61f4e01456d0dfb85637a88c2a391fa77
sunil2606 <44153134+sunil2606@users.noreply.github.com>
2018-10-25 23:12:31 +0530

    Add files via upload

---
+++ CODES FOR LOOPS/THE DO...WHILE STATEMENT.js
@@ -0,0 +1,14 @@
+<html>
+<body>
+<script type="text/javascript">
+var i=0
+do 
+{
+document.write("The number is " + i)
+document.write("<br />")
+i=i+1
+}
+while (i<0)
+</script>
+</body>
+</html>
---
+++ CODES FOR LOOPS/THE WHILE LOOP STATEMENT.js
@@ -0,0 +1,14 @@
+<html>
+<body>
+<script type="text/javascript">
+var i=0
+while (i<=10)
+{
+document.write("The number is " + i)
+document.write("<br />")
+i=i+1
+}
+
+</script>
+</body>
+</html>
